dbd10ff16d02325c8f4ae4387c0d9d599944c562
mvelkovska <mlvelkovska@gmail.com>
2021-03-22 23:24:45 +0100

    homework1

---
+++ Homework1/firstHomework/firstHomework/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace firstHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prvo choveche");
+            Console.WriteLine("     *  ");
+            Console.WriteLine("     *  ");
+            Console.WriteLine("  *  *  *");
+            Console.WriteLine("*    *    *");
+            Console.WriteLine("     *");
+            Console.WriteLine("     *");
+            Console.WriteLine("  *     *");
+            Console.WriteLine("*         *");
+
+
+            Console.WriteLine("Vtoro choveche");
+            Console.WriteLine("     *  ");
+            Console.WriteLine("     *  ");
+            Console.WriteLine("   * * *");
+            Console.WriteLine("  *  *  *");
+            Console.WriteLine("     *");
+            Console.WriteLine("     *");
+            Console.WriteLine("   *   *");
+            Console.WriteLine("  *     *");
+
+
+
+
+        }
+    }
+}
