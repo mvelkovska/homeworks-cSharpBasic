59ec04d3de1b02b27378768ae8ac394472909b6c
mvelkovska <mlvelkovska@gmail.com>
2021-04-27 23:00:02 +0200

    homework_05

---
+++ Homework05/Task1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Task1
+{
+    class Program
+    {
+
+  
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Queue<int> queueOfNumbers = new Queue<int>();
+                string userInput1;
+                string userInput2;
+                string number;
+                Console.WriteLine("How many numbers to you want to enter?");
+                userInput1 = Console.ReadLine();
+                bool checkNumber0 = int.TryParse(userInput1, out int parsedNumber);
+
+                if (checkNumber0)
+                {
+                    Console.WriteLine("Yes");
+                    for(int i = 0; i < parsedNumber; i++)
+                    {
+                        number = Console.ReadLine();
+                        bool checkNumber1 = int.TryParse(number, out int parsed);
+                        if (checkNumber1)
+                        {
+                            queueOfNumbers.Enqueue(parsed);
+
+                        }
+                    }
+
+                    Console.WriteLine("You have entered numbers:");
+
+                    foreach (int num in queueOfNumbers)
+                    {
+                        Console.WriteLine(num);
+                    }
+
+
+
+                        break;
+                }
+
+                else
+                {
+                    Console.WriteLine("You haven't entered a number.Enter any key to try again and x to exit.");
+                    userInput2 = Console.ReadLine();
+                    if (userInput2.ToLower() == "x")
+                    {
+                        Console.WriteLine("GOODBYE!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                    }
+
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
