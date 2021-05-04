49a47df14d73ef759571a03bbeb0cb02c246f5a6
mvelkovska <mlvelkovska@gmail.com>
2021-05-04 02:27:40 +0200

    homework_07

---
+++ Homework7/TaskDomain/Enums/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskDomain.Enums
+{
+    public enum Genre
+    {
+        Comedy = 1,
+        Horror,
+        Action,
+        Drama,
+        SciFi
+    }
+}
